53216631ecb8a778de61e30f214fff77f430f2e1
beneti <beneti@beneti.com.br>
2013-04-06 14:58:42 -0300

    log

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,18 @@ var conn = amqp.createConnection({url: url}, {defaultExchangeName: "amq.topic"})
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  conn.on('ready', function(){
+    console.log('Connected')
+    res.render('index', { title: 'Express' });
+  });
 };
 
 exports.talk = function(req, res){
-  setTimeout(function(){
-    conn.on('ready', function(){
-      var exchange = conn.exchange('');
-      var queue = conn.queue('queue1', {}, function() {
-        exchange.publish(queue.name, {body: req.body['message']}, function(){
-          res.write('ok');
-          res.end();
-        });
-      });
-    })
-  }, 5000);
+  var exchange = conn.exchange('amq.topic');
+  var queue = conn.queue('queue1', {}, function() {
+    exchange.publish(queue.name, {body: req.body['message']}, function(){
+      res.write('ok');
+      res.end();
+    });
+  });
 };
