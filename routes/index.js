ac43ff10e7b3c29ffb23f8ddab210adbcb063c50
beneti <beneti@beneti.com.br>
2013-04-06 14:11:21 -0300

    init

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var amqp = require('amqp');
+var url = process.env.CLOUDAMQP_URL || "amqp://localhost";
+var conn = amqp.createConnection({url: url});
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.talk = function(req, res){
+  conn.on('ready', function(){
+    var exchange = conn.exchange('');
+    var queue = conn.queue('queue1', {}, function() {
+      exchange.publish(queue.name, {body: req.body['message']}, function(){
+        res.write('ok');
+        res.end();
+      });
+    });
+  });
+};
